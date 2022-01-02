bedd6e009e21694815146d58f1d04ec8c7e1a4f6
bradley vangelder <r0784535@student.thomasmore.be>
2022-01-02 18:19:10 +0100

    fix

--- src/main/java/fact/it/demo/controller/MainController.java
+++ src/main/java/fact/it/demo/controller/MainController.java
@@ -10,15 +10,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.util.List;
-
-import static jdk.internal.net.http.HttpRequestImpl.USER_AGENT;
 
 @Controller
 public class MainController {
@@ -28,28 +24,6 @@ public class MainController {
 
     @RequestMapping("/")
     public String test(Model model) throws IOException {
-        URL url = new URL(edgeservicebaseurl + "/books");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("User-Agent", USER_AGENT);
-        int responseCode = con.getResponseCode();
-        System.out.println("GET Response Code :: " + responseCode);
-        if (responseCode == HttpURLConnection.HTTP_OK) { // success
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                    con.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            // print result
-            System.out.println(response.toString());
-        } else {
-            System.out.println("GET request not worked");
-        }
         model.addAttribute("name", "Bradley Vangelder");
         return "test";
     }
