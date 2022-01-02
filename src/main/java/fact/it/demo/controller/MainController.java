4fab551dc31ed2bfabbbceb8ae5d2cef5bbbfd24
bradley vangelder <r0784535@student.thomasmore.be>
2022-01-02 18:15:25 +0100

    adding get request

---
+++ src/main/java/fact/it/demo/controller/MainController.java
@@ -0,0 +1,56 @@
+package fact.it.demo.controller;
+
+import fact.it.demo.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.List;
+
+import static jdk.internal.net.http.HttpRequestImpl.USER_AGENT;
+
+@Controller
+public class MainController {
+
+    @Value("${edgeservice.baseurl}")
+    private String edgeservicebaseurl;
+
+    @RequestMapping("/")
+    public String test(Model model) throws IOException {
+        URL url = new URL(edgeservicebaseurl + "/books");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            System.out.println(response.toString());
+        } else {
+            System.out.println("GET request not worked");
+        }
+        model.addAttribute("name", "Bradley Vangelder");
+        return "test";
+    }
+}
---
+++ src/main/java/fact/it/demo/model/Book.java
@@ -0,0 +1,31 @@
+package fact.it.demo.model;
+
+public class Book {
+    private String ISBN;
+    private String title;
+    private String category;
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
