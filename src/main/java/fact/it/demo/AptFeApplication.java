171763b5871d532e9be45582ba716a1a44006a49
bradley vangelder <r0784535@student.thomasmore.be>
2022-01-02 17:14:26 +0100

    boilerplate

---
+++ src/main/java/fact/it/demo/AptFeApplication.java
@@ -0,0 +1,13 @@
+package fact.it.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptFeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AptFeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fact/it/demo/AptFeApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptFeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
